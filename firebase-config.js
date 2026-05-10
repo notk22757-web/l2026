d5a189e1a254ed2c7bd46aa22ce217d404e1611e
notk22757-web <notk22757@gmail.com>
2026-05-11 00:05:28 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,30 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDfGFzOAsArdWCVYnMFEwggAK-PQBjuor0",
+  apiKey: "AIzaSyDfGFz0AsArdWCVYnMFEwggAK-PQBjuor0",
   authDomain: "cbttest-d46ea.firebaseapp.com",
   projectId: "cbttest-d46ea",
   storageBucket: "cbttest-d46ea.firebasestorage.app",
   messagingSenderId: "881028247098",
   appId: "1:881028247098:web:06e5dd2c3f884eb7c80f03",
   measurementId: "G-BXJST6GRDT"
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+// Compatibility wrapper so app.js can use db.collection(...).add(...)
+window.db = {
+  collection(name) {
+    return {
+      add(data) {
+        return addDoc(collection(firestore, name), data);
+      }
+    };
+  }
 };
